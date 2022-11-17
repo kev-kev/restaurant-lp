04e9c2a19f4fb63caccb07c6536a7e586278860f
kev-kev <kevin.clark93@gmail.com>
2022-11-17 14:58:34 -0500

    Adding AOS and basic page elements

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaVersion": 2018
+
+    },
+    "rules": {
+    }
+}
