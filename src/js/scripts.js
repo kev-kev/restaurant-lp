0aa9fd0e95e1535486c94141e31b1c25d1cb3bf4
kev-kev <kevin.clark93@gmail.com>
2022-11-26 11:49:10 -0500

    Add sticky scrolling to topnav

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,15 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+
 AOS.init();
+
+const navbar = document.getElementById("navbar-container");
+const sticky = (window.screen.availHeight / 2) - navbar.offsetHeight;
+window.onscroll = function() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
+
