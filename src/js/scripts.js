08295af5d8c8211aea6a324a9c2739663894f688
kev-kev <kevin.clark93@gmail.com>
2022-12-01 19:16:10 -0500

    redesign navbar and style

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,14 +2,3 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 AOS.init();
-
-const navbar = document.getElementById("navbar-container");
-const headerImage = document.getElementById("header-background-image");
-window.onscroll = function() {
-  if (window.pageYOffset >= headerImage.offsetHeight - navbar.offsetHeight) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-};
-
