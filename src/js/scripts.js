6ac46859e522759451170a6a708dc3fc3e8bc94f
kev-kev <kevin.clark93@gmail.com>
2022-12-02 13:37:18 -0500

    Creating image slideshow

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,20 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import $ from 'jquery';
+import 'slick-carousel';
+
 
 AOS.init();
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   document.getElementsByClassName('slideshow')[0].slick({
+//     autoplay: true,
+//   })
+// })
+
+$(document).ready(() => {
+  console.log("hello");
+  $('.slideshow').slick({
+    autoplay: true
+  })
+});
